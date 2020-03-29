d22611245d14a4cb623499b0283364c85f754b08
mackowit <mackowit@o2.pl>
2020-03-29 19:12:59 +0200

    24.3 AOP.2

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/api/OrderFacadeWatcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/api/OrderFacadeWatcher.java
@@ -1,4 +1,18 @@
 package com.kodilla.kodillapatterns2.facade.api;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class OrderFacadeWatcher {
+    public static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.kodillapatterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("Logging processOrder method invocation");
+    }
 }
