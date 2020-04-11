088018146202ce2b0627e0f463da1a19bf67dc31
mackowit <mackowit@o2.pl>
2020-04-11 09:42:44 +0200

    31.2 Selenium

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/simpleinvoice/SimpleProduct.java
@@ -0,0 +1,28 @@
+package com.kodilla.testing2.simpleinvoice;
+
+public class SimpleProduct {
+    private final String productName;
+    private final double productPrice;
+
+    public SimpleProduct(String productName, double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SimpleProduct)) return false;
+        SimpleProduct that = (SimpleProduct) o;
+        return getProductName().equals(that.getProductName());
+    }
+
+}
